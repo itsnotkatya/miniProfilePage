f7b5f985f555f2f55127ae50785157343200548d
itsnotkatya <49903775+itsnotkatya@users.noreply.github.com>
2020-11-16 01:56:05 +1000

    initial commit

---
+++ func.js
@@ -0,0 +1,33 @@
+function myFunction() {
+    document.getElementById("pill-blue-pill").style.display = "none";
+
+    document.getElementById("my-info").style.display = "block";
+}
+
+function myFunction2() {
+    document.getElementById("my-info").style.display = "none";
+
+    document.getElementById("pill-blue-pill").style.display = "block";
+}
+
+function myFunction3() {
+    document.getElementById("pill-blue-pill").style.display = "none";
+
+    document.getElementById("pill-red-pill").style.display = "none";
+
+    document.getElementById("my-info").style.display = "none";
+
+    document.getElementById("matrix-box").style.display = "block";
+
+}
+
+function myFunction4() {
+    document.getElementById("matrix-box").style.display = "none";
+
+    document.getElementById("pill-red-pill").style.display = "block";
+
+    document.getElementById("pill-blue-pill").style.display = "block";
+
+}
+
+
